3c6dd90bf27d1a40bfa3a681a343a3448bd05045
Marcos Gaeta <marcos.gaeta@sentry.io>
2020-06-22 15:57:14 -0700

    Add a test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,27 +1,39 @@
-import {wait} from '../src/wait'
-import * as process from 'process'
-import * as cp from 'child_process'
-import * as path from 'path'
+import * as cp from 'child_process';
+import * as path from 'path';
+import * as process from 'process';
+import {getShouldFinalize} from '../src/validate';
 
-test('throws invalid number', async () => {
-  const input = parseInt('foo', 10)
-  await expect(wait(input)).rejects.toThrow('milliseconds not a number')
-})
+describe('validate', () => {
+  describe('getShouldFinalize', () => {
+    afterEach(() => {
+      delete process.env['INPUT_SKIP_FINALIZE'];
+    });
 
-test('wait 500 ms', async () => {
-  const start = new Date()
-  await wait(500)
-  const end = new Date()
-  var delta = Math.abs(end.getTime() - start.getTime())
-  expect(delta).toBeGreaterThan(450)
-})
+    test('should throw an error when skip_finalize is invalid', async () => {
+      process.env['INPUT_SKIP_FINALIZE'] = 'error';
+      expect(() => getShouldFinalize()).toThrow(
+        'skip_finalize is not a boolean'
+      );
+    });
+
+    test('should return true when skip_finalize is omitted', async () => {
+      expect(getShouldFinalize()).toBe(true);
+    });
+
+    test('should return false when skip_finalize is true', () => {
+      process.env['INPUT_SKIP_FINALIZE'] = 'true';
+      expect(getShouldFinalize()).toBe(false);
+    });
+  });
+});
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = '500'
-  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  process.env['INPUT_ENVIRONMENT'] = 'production';
+  process.env['MOCK'] = 'true';
+  const ip = path.join(__dirname, '..', 'lib', 'main.js');
   const options: cp.ExecSyncOptions = {
-    env: process.env
-  }
-  console.log(cp.execSync(`node ${ip}`, options).toString())
-})
+    env: process.env,
+  };
+  console.log(cp.spawnSync('node', [ip], options).toString());
+});
--- src/cli.ts
+++ src/cli.ts
@@ -1,10 +1,23 @@
 import SentryCli, {Releases} from '@sentry/cli';
 
-/** CLI Singleton */
+/**
+ * CLI Singleton
+ *
+ * When the `MOCK` environment variable is set, stub out network calls.
+ */
 let cli: Releases;
 export const getCLI = (): Releases => {
   if (!cli) {
     cli = new SentryCli().releases;
+    if (process.env['MOCK']) {
+      cli.execute = async (
+        args: string[],
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        live: boolean
+      ): Promise<string> => {
+        return Promise.resolve(args.join(' '));
+      };
+    }
   }
   return cli;
 };
